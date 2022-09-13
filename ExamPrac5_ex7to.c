72962abc5217cb568f7f74b3b83859f327adfaa2
Scorps0530 <blueandgreen0530@gmail.com>
2022-09-13 11:06:13 +0900

    haha

---
+++ ExamPrac5_ex7to.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main() {
+
+
+	// 주의
+	int i = 10;
+	int* pi = &i;
+	printf("%d %p\n", i, pi); // 10 0000008677EFF8F4
+	(*pi)++;
+	printf("%d %p\n", i, pi); // 11 0000008677EFF8F4
+	*pi++; // *(pi++)과 같음
+	printf("%d %p\n", i, pi); // 11 0000008677EFF8F8
+	printf("%d %p\n", i, pi); // 11 0000008677EFF8F8
+
+	// 주의
+	int x[5] = { 10, 20, 30, 40, 50 };
+	int* p = x;
+	p[0] = 20; // 배열처럼 인덱스로 접근하여 값을 변경할 수 있음.
+	printf("%d", x[0]);
+
+
+
+}
